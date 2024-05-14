ed4d35857d81b4ffa1ec02633298bc17e3ec0d8d
MikoBiko0609 <mad3566@icloud.com>
2024-05-13 20:15:28 -0500

    trying to seperate headers and footers into their own compoennts to clean up all files

---
+++ methods/loadHeaderFooter.js
@@ -0,0 +1,17 @@
+function loadHeaderFooter() {
+    fetch('/HeaderFooter/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+
+    fetch('/HeaderFooter/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
+}
+
+window.onload = loadHeaderFooter;
